4ac78c4543527a2fb8ec21001ad5b43d8b2b0d71
James Thompson <james@thomps.onl>
2020-12-03 21:58:44 -0700

    Frames can report whether they were left open.

--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -4,4 +4,28 @@ RSpec.describe Frame do
       expect(subject.rolls).to eq([])
     end
   end
+
+  describe "was the frame left open?" do
+    context "when less than ten pins were knocked down in all rolls" do
+      before do
+        subject.roll(3)
+        subject.roll(5)
+      end
+
+      it "reports true" do
+        expect(subject.open?).to be_truthy
+      end
+    end
+
+    context "when ten pens were knocked down in all rolls" do
+      before do
+        subject.roll(5)
+        subject.roll(5)
+      end
+
+      it "reports false" do
+        expect(subject.open?).to be_falsey
+      end
+    end
+  end
 end
